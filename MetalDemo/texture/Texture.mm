f1648aa9443e0b5083d9c11efb5867af17af2341
Roman Kuznetsov <r.kuznetsow@gmail.com>
2015-02-07 13:34:57 +0300

    Some refactoring of texture class

--- MetalDemo/texture/Texture.mm
+++ MetalDemo/texture/Texture.mm
@@ -19,10 +19,19 @@
     uint32_t _height;
     uint32_t _depth;
     MTLPixelFormat _format;
-    NSString *_path;
+    NSArray *_paths;
     BOOL _isReady;
     BOOL _mipMapsGenerated;
 }
+
+struct ImageData
+{
+    unsigned int width;
+    unsigned int height;
+    const void* pixels;
+    CGContextRef context;
+    ImageData() : width(0), height(0), pixels(0), context(0){}
+};
 
 - (instancetype) initWithResourceName:(NSString *)name
                             Extension:(NSString *)ext
@@ -36,22 +45,22 @@
     self = [super init];
     if(self)
     {
-        _path = path;
+        _paths = [NSArray arrayWithObject:path];
         _width = 0;
         _height = 0;
         _depth = 1;
         _format = MTLPixelFormatRGBA8Unorm;
         _target = MTLTextureType2D;
         _texture = nil;
-        _isReady = false;
-        _mipMapsGenerated = false;
+        _isReady = NO;
+        _mipMapsGenerated = NO;
     }
     return self;
 }
 
 - (void) dealloc
 {
-    _path = nil;
+    _paths = nil;
     _texture = nil;
     _isReady = false;
 }
@@ -72,59 +81,83 @@
     return [self loadFromFileWithDevice:device];
 }
 
-- (BOOL) loadFromFileWithDevice:(id <MTLDevice>)device
++ (ImageData) loadImageData:(NSString*)path
 {
-    UIImage *image = [UIImage imageWithContentsOfFile:_path];
-    if(!image) return NO;
+    ImageData result;
+    
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
+    if(!image) return ImageData();
     
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    if(!colorSpace) return NO;
+    if(!colorSpace) return ImageData();
     
-    _width  = uint32_t(CGImageGetWidth(image.CGImage));
-    _height = uint32_t(CGImageGetHeight(image.CGImage));
-    uint32_t rowBytes = _width * 4;
+    result.width  = uint32_t(CGImageGetWidth(image.CGImage));
+    result.height = uint32_t(CGImageGetHeight(image.CGImage));
+    uint32_t rowBytes = result.width * 4;
     
-    CGContextRef context = CGBitmapContextCreate(NULL, _width, _height, 8, rowBytes, colorSpace,
+    CGContextRef context = CGBitmapContextCreate(0, result.width, result.height, 8, rowBytes, colorSpace,
                                                  CGBitmapInfo(kCGImageAlphaPremultipliedLast));
     
     CGColorSpaceRelease(colorSpace);
-    if(!context) return NO;
+    if(!context) return ImageData();
     
-    CGRect bounds = CGRectMake(0.0f, 0.0f, _width, _height);
+    CGRect bounds = CGRectMake(0.0f, 0.0f, result.width, result.height);
     CGContextClearRect(context, bounds);
     CGContextDrawImage(context, bounds, image.CGImage);
     
+    result.pixels = CGBitmapContextGetData(context);
+    if(result.pixels == 0)
+    {
+        CGContextRelease(context);
+        return ImageData();
+    }
+    
+    result.context = context;
+    return result;
+}
+
++ (void) releaseImageData:(ImageData&)imageData
+{
+    if (imageData.context != 0)
+    {
+        CGContextRelease(imageData.context);
+        imageData.context = 0;
+    }
+    imageData.pixels = 0;
+}
+
+- (BOOL) loadFromFileWithDevice:(id <MTLDevice>)device
+{
+    ImageData imageData = [Texture loadImageData:_paths[0]];
+    if (imageData.pixels == 0) return NO;
+    
+    _width = imageData.width;
+    _height = imageData.height;
     MTLTextureDescriptor *texDesc = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:_format
                                                                                        width:_width
                                                                                       height:_height
                                                                                    mipmapped:YES];
-    if(!texDesc)
+    if (!texDesc)
     {
-        CGContextRelease(context);
+        [Texture releaseImageData:imageData];
         return NO;
     }
 
     _target = texDesc.textureType;
     _texture = [device newTextureWithDescriptor:texDesc];
-    if(!_texture)
+    if (!_texture)
     {
-        CGContextRelease(context);
+        [Texture releaseImageData:imageData];
         return NO;
     }
     
-    const void *pixels = CGBitmapContextGetData(context);
-    if(pixels != NULL)
-    {
-        MTLRegion region = MTLRegionMake2D(0, 0, _width, _height);
-        [_texture replaceRegion:region
-                    mipmapLevel:0
-                      withBytes:pixels
-                    bytesPerRow:rowBytes];
-    }
+    uint32_t rowBytes = _width * 4;
+    MTLRegion region = MTLRegionMake2D(0, 0, _width, _height);
+    [_texture replaceRegion:region mipmapLevel:0 withBytes:imageData.pixels bytesPerRow:rowBytes];
     
-    CGContextRelease(context);
-    
+    [Texture releaseImageData:imageData];
     _isReady = YES;
+    
     return YES;
 }
 
